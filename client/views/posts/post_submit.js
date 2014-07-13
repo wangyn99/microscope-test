c40aa24bdec0b3d1207a4e4aefeee95609d8ecda
kelly <wangyn99@outlook.com>
2014-07-13 21:08:16 +0800

    Meteor.methods

--- client/views/posts/post_submit.js
+++ client/views/posts/post_submit.js
@@ -6,8 +6,17 @@ Template.postSubmit.events({
 			title: $(e.target).find('[name=title]').val(),
 			message:$(e.target).find('[name=message]').val()
 		};
-		post_id = Posts.insert(post);
-		Router.go('postPage', {_id: post_id});
+		//post_id = Posts.insert(post);
+		//Meteor.call  function callls  a method 'post'.
+		//fistt argument: 'post' , a method
+		//second argument: post , a post object constructed fron the form
+		//third argument ; a callback function, will excute when the server-side method is done
+		Meteor.call('post', post, function(error, id){
+			if(error)
+				return alert(error.reason);
+			Router.go('postPage', {_id: id})
+		});
+		//Router.go('postPage', {_id: post_id});
 		//notice: must be {_id: post_id}, not "post" directly(it may lead to null id);
 	}
 })
