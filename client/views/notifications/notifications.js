1d8701c5f96c3a05d3bab8eb98ec5775b47bc65e
kelly <wangyn99@outlook.com>
2014-07-15 13:34:10 +0800

    nitifications

---
+++ client/views/notifications/notifications.js
@@ -0,0 +1,19 @@
+Template.notifications.helpers({
+	notifications: function(){
+		return Notifications.find({userId: Meteor.userId(),read: false});
+	},
+	notificationCount:function(){
+		
+		return  Notifications.find({userId: Meteor.userId(),read: false}).count();
+	}
+});
+Template.notifications.helpers({
+	notificationsPostPath: function(){
+		return Router.routes.postPage.path({_id:this.postId});
+	}
+})
+Template.notifications.events({
+	'click a':function(){
+		Notifications.update(this._id,{$set:{read:true}});
+	}
+})
